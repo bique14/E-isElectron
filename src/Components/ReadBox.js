8641eedda516be241fffbe11675adbba2573f6d4
bique14 <peerasorn14@gmail.com>
2018-10-14 23:35:58 +0700

    read file by input

--- src/Components/ReadBox.js
+++ src/Components/ReadBox.js
@@ -1,32 +1,51 @@
 import React from 'react'
 
-// const getCode = require('../utils/readFile.js')
-
 export class ReadBox extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      fileName: '',
       code: ''
     }
   }
-  componentDidMount() {
-    fetch('http://localhost:3003', {
-      method: 'GET',
-      mode: 'CORS',
+  // componentDidMount() {
+  //   fetch('http://localhost:3003', {
+  //     method: 'GET',
+  //     mode: 'CORS',
+  //     headers: {
+  //       'Content-Type': 'application/text',
+  //     }
+  //   })
+  //     .then(res => res.text())
+  //     .then(data => this.setState({ code: data }))
+  // }
+
+  submitFileName() {
+    const { fileName } = this.state
+    console.log(fileName)
+    fetch('http://localhost:3003/get-code', {
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/text',
-      }
-    })
-      .then(res => res.text())
-      .then(data => this.setState({ code: data }))
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        pathToFile: fileName
+      })
+    }).then(res => res.text())
+    .then(data => this.setState({ code: data }))
   }
 
   render() {
     const { code } = this.state
-    console.log(code)
+    // console.log(code)
     return (
-      <div style={{backgroundColor: 'white', color: 'black'}}>
-        {code}
+      <div >
+        <input type="text" onChange={(e) => this.setState({ fileName: e.target.value})}/>
+        <button onClick={this.submitFileName.bind(this)}>Submit</button>
+        <div style={{backgroundColor: 'white', color: 'black'}}>
+          {code}
+        </div>
       </div>
     )
   }
--- src/utils/readFile.js
+++ src/utils/readFile.js
@@ -1,30 +1,63 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-// const filePath = path.resolve(__dirname, '../Components/code/Read.js')
-// const code = fs.readFileSync(filePath).toString()
+// var http = require('http')
+// var port = 3003
+// var serverUrl = 'localhost'
 
-// console.log(code)
+// var server = http.createServer(function (req, res) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-var http = require('http')
-var port = 3003
-var serverUrl = 'localhost'
+//   const filePath = path.resolve(__dirname, '../Components/code/Test.js')
+//   const code = fs.readFileSync(filePath).toString()
+//   res.end(code)
+// })
 
-var server = http.createServer(function (req, res) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  
+// console.log('Listening at ' + serverUrl + ':' + port)
+// server.listen(port, serverUrl)
+
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.use(bodyParser.json())
+app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   const filePath = path.resolve(__dirname, '../Components/code/Test.js')
   const code = fs.readFileSync(filePath).toString()
   res.end(code)
 })
 
-console.log('Listening at ' + serverUrl + ':' + port)
-server.listen(port, serverUrl)
+app.post('/get-code', function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const r = req.body.pathToFile
+  console.log('server[post] /get-code') 
+  console.log(req.body, r) 
+
+  const filePath = path.resolve(__dirname, `../Components/code/${r}`)
+  const code = fs.readFileSync(filePath).toString()
+  res.end(code)
+
+})
+
+app.listen(3003, function () {
+  console.log('Example app listening on port 3003!')
+})
